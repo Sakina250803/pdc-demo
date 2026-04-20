b9da8ad64daf6be1d1e602a4302921d579a8dd5f
sakina250803 <bk682409@gmail.com>
2026-04-20 15:03:26 -0700

    Testing secret scanning

---
+++ code secrets_test.py
@@ -0,0 +1,6 @@
+# WARNING: Never do this in real code!
+API_KEY = "AKIAIOSFODNN7EXAMPLE"
+AWS_SECRET = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+DATABASE_PASSWORD = "super_secret_123"
+
+print("This file has exposed secrets!")
